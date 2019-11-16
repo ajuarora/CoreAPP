b64a9b34de3c0fcf5c2f376fb1b366335d26fb59
arjun <arjunsarora@hotmail.com>
2019-11-16 13:03:23 -0500

    End of Seven

--- App-SPA/src/app/app.module.ts
+++ App-SPA/src/app/app.module.ts
@@ -9,14 +9,28 @@ import { FormsModule } from "@angular/forms";
 import { HomeComponent } from "./home/home.component";
 import { RegisterComponent } from "./register/register.component";
 import { ErrorInterceptorProvider } from "./_services/error.interceptor";
+import { ListsComponent } from "./lists/lists.component";
+import { MemberListComponent } from "./member-list/member-list.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { RouterModule } from "@angular/router";
+import { appRoutes } from "./routes";
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    ListsComponent,
+    MemberListComponent,
+    MessagesComponent
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ErrorInterceptorProvider],
   bootstrap: [AppComponent]
